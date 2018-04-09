913a47ce423b6bc13451a7b87f560f10fa620dff
youjin kim <38131695+u0jin@users.noreply.github.com>
2018-04-09 21:06:23 +0900

    delete btn 수정

---
+++ _DB연동(0407)/_DB연동(0327)/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _DB연동_0327_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button.Location = new System.Drawing.Point(371, 102);
+            this.button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(86, 29);
+            this.button.TabIndex = 0;
+            this.button.Text = "검색";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Location = new System.Drawing.Point(57, 168);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 296);
+            this.dataGridView1.TabIndex = 2;
+           // this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter_1);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(133, 488);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 31);
+            this.updateButton.TabIndex = 5;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(300, 488);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 31);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 562);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ _DB연동(0407)/_DB연동(0327)/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _DB연동_0327_
+{
+   
+    public partial class Form1 : Form
+    {
+        Con_Database db;
+       int index=1; //전역변수 (Column의 index 선택하는)
+                    //Default 검색은 NAME
+
+       bool state = false; //전역변수로 bool state값 만든다. 
+       //rc값이 해당되지않았을경우에는 false 로 둔다.
+
+       object rc;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new Con_Database(this);
+         
+            db.ConnectDB();
+        }
+
+        
+        //index==0 -> ID순
+        //index==1 -> NAME순
+        //index==2 -> PhoneNumber순 
+        //index==3 -> Address순
+        //index==4 -> E-Mail순
+        private void button_Click(object sender, EventArgs e)  
+        {
+            string Mysql="";
+
+            if (index == 0)
+            {
+                Mysql = "SELECT * FROM data where id LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if(index==1)
+            {
+                Mysql = "SELECT * FROM data where name LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 2)
+            {
+                Mysql = "SELECT * FROM data where phone_number LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 3)
+            {
+                Mysql = "SELECT * FROM data where e_mail LIKE '%" + textBox1.Text + "%' ";
+            }
+            else if (index == 4)
+            {
+                Mysql = "SELECT * FROM data where address LIKE '%" + textBox1.Text + "%' ";
+            }
+
+            DataTable dt = db.GetDBTable(Mysql);
+            dataGridView1.DataSource = dt;
+            db.CloseDB();
+
+
+        }
+     
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {        //특정 키 버튼 처리-키를 처음 누를때 처리되는것
+
+            if (e.KeyChar == '\r')
+            {
+                button_Click(sender, e);    //이름 기준
+               // button_Click(null, null); 왜 둘이 같은 동작을 하는걸까요???
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { //간지용
+            button_Click(sender, e);        //이름 기준
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+              //  MessageBox(comboBox1.SelectedIndex.ToString());
+              //  MessageBox.Show(comboBox1.SelectedIndex.ToString());// 인덱스 출력
+                index = comboBox1.SelectedIndex;
+                //this.itemSelected = comboDropDown.SelectedItem as string;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] data = { "ID", "Name", "Phone_number", "E-mail", "Address" };
+            //comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Text = "Column";
+            comboBox1.Items.AddRange(data); //배열
+    
+        }
+
+        //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{ //현재셀의 row index
+        //    object rc = dataGridView1.CurrentCell.Value;  //for find the row index number
+        //    MessageBox.Show("Current Row Index is = " + rc.ToString());
+        //}
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        { //행을 선택-> 그 id값을 불러옴
+
+            rc = dataGridView1.CurrentCell.Value;  //for find the row index number
+          //  MessageBox.Show("Current Row Index is = " + rc.ToString());
+            state = true;
+            //rc에 값이 할당된경우 state가 true로 변경된다.
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //try catch문의 문제점 ? -> 처음에 rc값없이 delete 를 눌렀을경우 catch 로 끝나서 
+            //그다음 바로 rc 값을 할당하는 동작을 실행했을경우 할당이 안된다
+            //즉 프로그램 이 끝난거같다. (rc값을 넣어주는 동작이 끝난거같음)
+
+            
+           //try catch를 쓰지않으면 프로그램이 종료된다
+
+           
+
+            //try -catch 문을 썻을때 catch에서 잡히면 다시 try가 안된다 즉 delete버튼의 동작이 끝나는것
+            // 수정해야함 try-catch를 버리던가 다시 try할수있도록 수정하거나...
+          //   한번더 try catch try를 하기위해 while()문으로 돌리면 프로그램 응답대기가 뜬다
+
+            
+            //프로그램이 종료되지않고 계속 돌아가게 하기위해서 
+            //try catch를 버리고 if-else if로 변경한다.
+ 
+            if (state == true) //state 가 true로 변경된경우 즉,  rc값이 할당된경우
+            {
+                db.deleteFunc(rc);
+            }
+            
+                  
+            else if (state == false) 
+                //state가 true가 아닌경우 즉 rc에 값이 할당되지않았을경우  state는 false로 변경된다.
+             {
+               MessageBox.Show("Please click row header"); 
+             }
+               
+                
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        { //수정->저장 & 추가-> 저장
+    
+         
+                //추가 버튼
+                try
+                {
+                    db.addFunc();
+                   
+                }
+                catch (Exception e1)
+                {
+                    MessageBox.Show("Please click row header");
+                   
+                }
+            
+          }
+
+        //private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    girdview에서 셀을 추가할때 발생
+        //    db.add(new object[] { "string", 1, true, null });
+        //    dataGridView1.Rows.Add(1);
+        //}
+
+        //private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    dataGridView1.Rows.Add(1);
+        //}
+
+     
+
+    
+    }
+}
